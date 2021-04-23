edb4884d79651b76d2b645ebd32d0fc06dcaee5e
Sergey Semenov <s3menov.semenow@yandex.ru>
2021-04-24 00:27:32 +0300

    Created goods section

--- dist/js/script.js
+++ dist/js/script.js
@@ -9,20 +9,20 @@
 
 function testWebP(callback) {
 
-  var webP = new Image();
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-  };
-  webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
 }
 
 testWebP(function (support) {
 
-  if (support == true) {
-    document.querySelector('body').classList.add('webp');
-  } else {
-    document.querySelector('body').classList.add('no-webp');
-  }
+    if (support == true) {
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
+    }
 });
 
 
@@ -31,7 +31,7 @@ testWebP(function (support) {
 $('.main-slider__inner').slick({
     infinite: true,
     speed: 2000,
-    lazyLoad:'ondemand',
+    lazyLoad: 'ondemand',
     pauseOnHover: true,
     touchMove: false,
     swipe: true,
@@ -47,4 +47,27 @@ $('.main-slider__inner').slick({
     prevArrow: $('.main-slider__prev'),
     nextArrow: $('.main-slider__next'),
 
-  });
+});
+
+
+// =========== goods-slider ===============
+$('.goods__slider').slick({
+    infinite: true,
+    speed: 2000,
+    lazyLoad: 'ondemand',
+    pauseOnHover: true,
+    touchMove: false,
+    swipe: true,
+    pauseOnFocus: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    dots: true,
+    dotsClass: "goods__slider__numbers-container",
+    prevArrow: $('.goods__slider__prev'),
+    nextArrow: $('.goods__slider__next'),
+
+});
+
